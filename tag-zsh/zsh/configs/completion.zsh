71c155a20fac5c2be9d439dbb3f54b35188e719b
Oliver Davies <oliver@oliverdavies.uk>
2020-01-14 14:58:05 +0000

    Re-enable completion and case insensitivity

---
+++ tag-zsh/zsh/configs/completion.zsh
@@ -0,0 +1,6 @@
+autoload -U compinit
+compinit
+
+## case-insensitive (all), partial-word and then substring completion
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' \
+    'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
