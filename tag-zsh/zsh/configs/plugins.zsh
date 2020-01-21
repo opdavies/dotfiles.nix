ef5db89e75e57515bef544930080b939e6e36252
Oliver Davies <oliver@oliverdavies.uk>
2020-01-21 11:39:25 +0000

    Add zsh-users/zsh-completions

--- tag-zsh/zsh/configs/plugins.zsh
+++ tag-zsh/zsh/configs/plugins.zsh
@@ -2,6 +2,7 @@ source /usr/local/share/antigen/antigen.zsh
 
 antigen bundle mollifier/cd-gitroot
 antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle zsh-users/zsh-completions
 antigen bundle zsh-users/zsh-syntax-highlighting
 
 antigen apply
