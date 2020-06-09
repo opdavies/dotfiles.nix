211ea05c0e2b89254805b47977d84a23c04f677c
Oliver Davies <oliver@oliverdavies.uk>
2020-06-09 21:24:12 +0100

    Don't connect to tmux in PhpStorm terminal

--- tag-zsh/zsh/configs/tmux.zsh
+++ tag-zsh/zsh/configs/tmux.zsh
@@ -2,8 +2,16 @@ _not_inside_tmux() {
   [[ -z "$TMUX" ]]
 }
 
+_not_inside_phpstorm() {
+  if is_macos; then
+    [[ "$TERM_PROGRAM" != "" ]]
+  elif is_linux; then
+    [[ "$TERMINAL_EMULATOR" != "JetBrains"* ]]
+  fi
+}
+
 ensure_tmux_is_running() {
-  if _not_inside_tmux; then
+  if _not_inside_tmux && _not_inside_phpstorm; then
     tat
   fi
 }
