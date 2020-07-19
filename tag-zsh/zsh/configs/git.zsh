0f797c6ffeb255b1e8f9ef9eb4c1fd905e311370
Oliver Davies <oliver@oliverdavies.uk>
2020-07-19 21:58:21 +0100

    Prefer "main" to "master"
    See
    https://github.com/gabebw/dotfiles/commit/1e7fa50da004e10436d1b2145054cd08172afd05.
    
    > This commit introduces `git-master-to-main-wrapper`, which seamlessly
    prefers `main` to `master` but works fine with repos that do use
    a `master` branch.

--- tag-zsh/zsh/configs/git.zsh
+++ tag-zsh/zsh/configs/git.zsh
@@ -1,7 +1,14 @@
 # No arguments: `git status`
 # With arguments: acts like `git`
 g() {
-  if [[ $# > 0 ]]; then
+  if [[ $1 == init ]];
+  then
+    # Prefer "main" to "master"
+    git "$@"
+    git checkout -b main
+    git branch -d master
+  elif [[ $# > 0 ]];
+  then
     git $@
   else
     git status --short --branch
