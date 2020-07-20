b35d9a938ad39727afc7ae7eaead2567f4eed46a
Oliver Davies <oliver@oliverdavies.uk>
2020-07-20 01:47:30 +0100

    git: Add an init template, use it to create the main branch
    See
    https://github.com/thoughtbot/dotfiles/commit/56d614f8067c275f4d7b53f3f3a30a5effbeded9.
    
    > This change adds a HEAD file to the Git template with a different ref.
    This means that new repositories will commit to a "main" branch by
    default instead.

--- tag-zsh/zsh/configs/git.zsh
+++ tag-zsh/zsh/configs/git.zsh
@@ -1,13 +1,7 @@
 # No arguments: `git status`
 # With arguments: acts like `git`
 g() {
-  if [[ $1 == init ]];
-  then
-    # Prefer "main" to "master"
-    git "$@"
-    git checkout -b main
-    git branch -d master
-  elif [[ $# > 0 ]];
+  if [[ $# > 0 ]];
   then
     git $@
   else
