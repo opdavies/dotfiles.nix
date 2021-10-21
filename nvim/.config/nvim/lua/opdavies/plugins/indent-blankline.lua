1473b4da61f42ed12ce8310d53c7823789ed4455
Oliver Davies <oliver@oliverdavies.uk>
2021-10-21 22:44:05 +0100

    nvim: set colour for indent lines

--- nvim/.config/nvim/lua/opdavies/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/opdavies/plugins/indent-blankline.lua
@@ -3,6 +3,8 @@ vim.opt.listchars = {
   eol = "↴",
 }
 
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#555555 gui=nocombine]]
+
 require("indent_blankline").setup {
   char_highlight_list = {
     "IndentBlanklineIndent1",
