3c0d75b7e6b0e5e1db78b930bf14c60fed71fd54
Oliver Davies <oliver@oliverdavies.uk>
2021-10-30 12:05:09 +0100

    refactor(nvim): move vim options into lua

--- nvim/.config/nvim/lua/opdavies/init.lua
+++ nvim/.config/nvim/lua/opdavies/init.lua
@@ -1,3 +1,29 @@
+vim.g.mapleader = ' '
+
+vim.cmd('filetype indent on')
+vim.cmd('filetype on')
+vim.cmd('filetype plugin on')
+vim.cmd('syntax on')
+
+vim.opt.autoindent = true
+vim.opt.breakindent = true
+vim.opt.clipboard:append 'unnamedplus'
+vim.opt.expandtab = true
+vim.opt.foldmethod = 'indent'
+vim.opt.formatoptions = 'lm'
+vim.opt.linebreak = true
+vim.opt.mouse = 'n'
+vim.opt.number = true
+vim.opt.scrolloff = 10
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.softtabstop = 2
+vim.opt.swapfile = false
+vim.opt.tabstop = 2
+vim.opt.termguicolors = true
+vim.opt.updatetime = 1000
+vim.opt.wrap = true
+
 require "opdavies.plugins.colours"
 require "opdavies.plugins.completion"
 require "opdavies.plugins.floaterm"
