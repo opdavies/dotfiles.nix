04392ea377dfd8b878d6e61d9695f7794c397b29
Oliver Davies <oliver@oliverdavies.uk>
2021-11-17 21:50:32 +0000

    feat: experimenting with custom Telescope commands

---
+++ nvim/.config/nvim/lua/opdavies/telescope/mappings.lua
@@ -0,0 +1,32 @@
+local M = {}
+
+function M.edit_neovim()
+  local opts = {
+    prompt_title = "~ dotfiles ~",
+    shorten_path = false,
+    cwd = "~/.config/nvim",
+
+    layout_strategy = "flex",
+    layout_config = {
+      width = 0.9,
+      height = 0.8,
+
+      horizontal = {
+        width = { padding = 0.15 },
+      },
+      vertical = {
+        preview_height = 0.75,
+      },
+    },
+  }
+
+  require("telescope.builtin").find_files(opts)
+end
+
+function M.fd()
+  local themes = require "telescope.themes"
+
+  require("telescope.builtin").find_files(themes.get_ivy())
+end
+
+return M
