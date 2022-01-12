a44b5b092f909a79c6b35db7cc1c3481d2022048
Oliver Davies <oliver@oliverdavies.uk>
2022-01-12 07:43:19 +0000

    feat(nvim): add `drupalclass` snip

--- roles/neovim/files/lua/opdavies/snippets/ft/php.lua
+++ roles/neovim/files/lua/opdavies/snippets/ft/php.lua
@@ -7,6 +7,27 @@ local s = luasnip.snippet
 local t = luasnip.text_node
 
 return {
+  -- TODO: only load if within a Drupal project?
+  s(
+    "drupalclass",
+    fmta(
+      [[
+      <<?php
+
+      declare(strict_types=1);
+
+      namespace Drupal\<>;
+
+      <>class <> {
+
+        <>
+
+      }
+      ]],
+      { i(1), c(2, { t '', t 'final ' }), i(3), i(0) }
+    )
+  ),
+
   s(
     "test",
     fmta(
