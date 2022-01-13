a4db5be33fe6ed3bd8dac0886869187de45dd9a9
Oliver Davies <oliver@oliverdavies.uk>
2022-01-13 00:32:16 +0000

    fix(nvim): change sorting strategy when grepping

--- roles/neovim/files/lua/opdavies/telescope/init.lua
+++ roles/neovim/files/lua/opdavies/telescope/init.lua
@@ -57,7 +57,7 @@ map(
 )
 map("n", "<leader>fg", '<CMD>lua require("telescope.builtin").git_files{}<CR>', options)
 map("n", "<leader>fh", '<CMD>lua require("telescope.builtin").help_tags()<CR>', options)
-map("n", "<leader>fl", '<CMD>lua require("telescope.builtin").live_grep()<CR>', options)
+map("n", "<leader>fl", '<CMD>lua require("telescope.builtin").live_grep({ sorting_strategy="ascending" })<CR>', options)
 map("n", "<leader>fr", '<CMD>lua require("telescope.builtin").registers()<CR>', options)
 
 -- Custom
