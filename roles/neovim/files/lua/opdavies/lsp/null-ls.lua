776772a952a7ae97485d6040702411251d418fa3
Oliver Davies <oliver@oliverdavies.uk>
2022-01-20 18:00:00 +0000

    chore(nvim): disable auto-formatting
    This is tricky when working on projects that use different formatting
    and coding styles (e.g. Drupal and Symfony), and on projects that
    haven't had code formatting run on them before and making a small change
    causes a large number of changes to be staged.
    
    I'll experiment with formatting code within a range, or toggling
    auto-formatting per project. Maybe something similar to what @wincent
    showed in https://youtu.be/ifA8iw42vwI.

--- roles/neovim/files/lua/opdavies/lsp/null-ls.lua
+++ roles/neovim/files/lua/opdavies/lsp/null-ls.lua
@@ -19,16 +19,5 @@ null_ls.setup {
     diagnostics.phpstan,
 
     completion.spell,
-  },
-
-  on_attach = function(client)
-    if client.resolved_capabilities.document_formatting then
-      vim.cmd [[
-        augroup LspFormatting
-          autocmd! * <buffer>
-          autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
-        augroup END
-      ]]
-    end
-  end,
+  }
 }
