f6578a821aeef3dcfc504b517ebde531b2959f65
Oliver Davies <oliver@oliverdavies.uk>
2022-02-16 08:18:32 +0000

    feat(nvim): add a snippet for `pcall`
    Useful when adding configuration files for new plugins.

--- roles/neovim/files/lua/opdavies/snippets/ft/lua.lua
+++ roles/neovim/files/lua/opdavies/snippets/ft/lua.lua
@@ -5,5 +5,12 @@ local rep = require("luasnip.extras").rep
 local i = ls.insert_node
 
 return {
-  req = fmt("local {} = require('{}')", { i(1), rep(1) })
+  req = fmt("local {} = require('{}')", { i(1), rep(1) }),
+
+  pcall = fmt([[
+    local status_ok, {} = pcall(require, "{}")
+    if not status_ok then
+      return
+    end
+  ]], { i(1), rep(1) })
 }
