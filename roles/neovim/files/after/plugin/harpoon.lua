25c6a44a2ab18bd3640db8908c891ee43076d2eb
Oliver Davies <oliver@oliverdavies.uk>
2022-03-23 11:43:05 +0000

    feat(nvim): configure harpoon

---
+++ roles/neovim/files/after/plugin/harpoon.lua
@@ -0,0 +1,20 @@
+local status_ok, harpoon = pcall(require, "harpoon")
+if not status_ok then
+  return
+end
+
+local nmap = require("opdavies.keymap").nmap
+
+harpoon.setup {}
+
+nmap { "<M-h><M-l>", require("harpoon.ui").toggle_quick_menu }
+nmap { "<M-h><M-m>", require("harpoon.mark").add_file }
+
+for i = 1, 5 do
+  nmap {
+    string.format("<space>%s", i),
+    function()
+      require("harpoon.ui").nav_file(i)
+    end,
+  }
+end
