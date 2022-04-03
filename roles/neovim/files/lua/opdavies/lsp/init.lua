6c95ee5d4fdd64d63022675ddd6cc9912d50d450
Oliver Davies <oliver@oliverdavies.uk>
2022-04-03 21:43:56 +0100

    chore(nvim): add leader before mappings

--- roles/neovim/files/lua/opdavies/lsp/init.lua
+++ roles/neovim/files/lua/opdavies/lsp/init.lua
@@ -38,13 +38,12 @@ local custom_attach = function(client)
 
   buf_nnoremap { "<leader>dn", vim.diagnostic.goto_next }
   buf_nnoremap { "<leader>dp", vim.diagnostic.goto_prev }
+  buf_nnoremap { "<leader>gD", vim.lsp.buf.declaration }
+  buf_nnoremap { "<leader>gT", vim.lsp.buf.type_definition }
+  buf_nnoremap { "<leader>gd", vim.lsp.buf.definition }
   buf_nnoremap { "<leader>rn", vim.lsp.buf.rename }
   buf_nnoremap { "<leader>rr", "<cmd>LspRestart<cr>" }
-
   buf_nnoremap { "K", vim.lsp.buf.hover }
-  buf_nnoremap { "gD", vim.lsp.buf.declaration }
-  buf_nnoremap { "gT", vim.lsp.buf.type_definition }
-  buf_nnoremap { "gd", vim.lsp.buf.definition }
 
   if filetype ~= "lua" then
     buf_nnoremap { "K", vim.lsp.buf.hover }
