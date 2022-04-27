f9f9c0a5e82859fb830a00053fafb5e47aa2e7ea
Oliver Davies <oliver@oliverdavies.uk>
2022-04-27 13:41:37 +0100

    refactor(zsh): update aliases

--- roles/zsh/files/.zsh/configs/aliases.zsh
+++ roles/zsh/files/.zsh/configs/aliases.zsh
@@ -1,18 +1,8 @@
-alias c="clear"
-alias l="ls -lah"
-
-# Open the current directory in Finder.
-alias o="open ."
-
-alias hosts="sudo vim /etc/hosts"
+alias hosts='sudo vim /etc/hosts'
+alias rebase='git fetch --all --prune --prune-tags && git rebase'
+alias run='./run'
 alias sshconfig='vim ~/.ssh/config'
-
-# Pretty print the path.
-alias path='echo $PATH | tr -s ":" "\n"'
-
-# Open the current directory in PhpStorm.
-# Open the current directory in Sublime Text.
-alias sublime='open -a /Applications/Sublime\ Text.app "`pwd`"'
+alias vim='nvim'
 
 # Allow for pasting lines of code that start with a dollar sign.
 alias \$=''
@@ -20,13 +10,3 @@ alias \$=''
 # Re-implement pbcopy and pbpaste from macOS.
 alias pbcopy="xclip -selection clipboard"
 alias pbpaste="xclip -selection clipboard -o"
-
-# Include custom aliases
-if [[ -f ~/.aliases.local ]]; then
-  source ~/.aliases.local
-fi
-
-# Open directory aliases
-if [[ -f $(pwd)/.aliases ]]; then
-  source $(pwd)/.aliases
-fi
