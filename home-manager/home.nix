cb812ee0632d30621d0c1bd27ec16732d857f9db
Oliver Davies <oliver@oliverdavies.uk>
2022-09-22 12:12:02 +0100

    fix(tmux): syntax error in tmux.conf

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -129,8 +129,8 @@
       set-option -ga terminal-overrides ",*256col*:Tc"
       set-option -g default-terminal "xterm-256color"
 
-      bind-key h split-window -v  -c '#{pane_current_path}'
-      bind-key v split-window -h  -c '#{pane_current_path}'
+      bind-key h split-window -v  -c "#{pane_current_path}"
+      bind-key v split-window -h  -c "#{pane_current_path}"
 
       bind -n S-Left resize-pane -L 2
       bind -n S-Right resize-pane -R 2
@@ -166,27 +166,9 @@
 
       bind-key K run-shell 'tmux switch-client -n \; kill-session -t "$(tmux display-message -p "#S")" || tmux kill-session'
 
-      # Smart pane switching with awareness of Vim splits.
-      # See: https://github.com/christoomey/vim-tmux-navigator
-      is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
-          | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|n?vim?x?)(diff)?$'"
-      bind-key -n 'C-h' if-shell "$is_vim" 'send-keys C-h'  'select-pane -L'
-      bind-key -n 'C-j' if-shell "$is_vim" 'send-keys C-j'  'select-pane -D'
-      bind-key -n 'C-k' if-shell "$is_vim" 'send-keys C-k'  'select-pane -U'
-      bind-key -n 'C-l' if-shell "$is_vim" 'send-keys C-l'  'select-pane -R'
-      tmux_version='$(tmux -V | sed -En "s/^tmux ([0-9]+(.[0-9]+)?).*/\1/p")'
-      if-shell -b '[ "$(echo "$tmux_version < 3.0" | bc)" = 1 ]' \
-          "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\'  'select-pane -l'"
-      if-shell -b '[ "$(echo "$tmux_version >= 3.0" | bc)" = 1 ]' \
-      bind-key -T copy-mode-vi 'C-h' select-pane -L
-      bind-key -T copy-mode-vi 'C-j' select-pane -D
-      bind-key -T copy-mode-vi 'C-k' select-pane -U
-      bind-key -T copy-mode-vi 'C-l' select-pane -R
-      bind-key -T copy-mode-vi 'C-\' select-pane -l
-
       # Allow clearing screen with ctrl-l by using <prefix> C-l
-      bind C-l send-keys 'C-l'
-      bind C-k send-keys 'C-k'
+      bind C-l send-keys "C-l"
+      bind C-k send-keys "C-k"
 
       # Enable mouse support.
       setw -g mouse on
