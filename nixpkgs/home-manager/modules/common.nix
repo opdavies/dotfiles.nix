ffdbb50f01e34abd6d78c16dc7454eaba8098464
Oliver Davies <oliver@oliverdavies.uk>
2022-09-29 20:59:38 +0100

    feat: install LSP and formatters

--- nixpkgs/home-manager/modules/common.nix
+++ nixpkgs/home-manager/modules/common.nix
@@ -1,10 +1,30 @@
 { config, lib, pkgs, ... }:
 {
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "intelephense"
+  ];
+
   home.packages = with pkgs; [
     just
 
     # LSP
+    # TODO: nodePackages."@ansible/ansible-language-server"
+    nodePackages."@tailwindcss/language-server"
+    nodePackages.bash-language-server
+    nodePackages.dockerfile-language-server-nodejs
+    nodePackages.eslint
+    nodePackages.intelephense
     nodePackages.markdownlint-cli
+    nodePackages.prettier
+    nodePackages.typescript
+    nodePackages.typescript-language-server
+    nodePackages.vls
+    nodePackages.vscode-langservers-extracted
+    nodePackages.yaml-language-server
+    php81Packages.phpcbf
+    php81Packages.phpcs
+    php81Packages.phpstan
+    shellcheck
   ];
 
   programs.bat.enable = true;
