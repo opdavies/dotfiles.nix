8798a82bbe928c2c5d9411383a4a517e60f32e80
Oliver Davies <oliver@oliverdavies.uk>
2022-10-04 23:11:23 +0100

    fix(nixos): enable experimental features

--- nixpkgs/nixos/apollo/configuration.nix
+++ nixpkgs/nixos/apollo/configuration.nix
@@ -13,6 +13,8 @@ in
       ./hardware-configuration.nix
     ];
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
