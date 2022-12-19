a2dc676a42480085a7f51c4a79801a50301998e0
Oliver Davies <oliver@oliverdavies.uk>
2022-12-19 09:42:03 +0000

    feat: add `t` keybindings for Tmux

--- nixpkgs/home-manager/modules/tmux.nix
+++ nixpkgs/home-manager/modules/tmux.nix
@@ -90,6 +90,9 @@
       bind-key -T copy-mode-vi 'C-k' select-pane -U
       bind-key -T copy-mode-vi 'C-l' select-pane -R
       bind-key -T copy-mode-vi 'C-\' select-pane -l
+
+      bind-key -r F new-window t
+      bind-key -r D run-shell "t ~/Code/dotfiles"
     '';
 
     plugins = with pkgs; [
