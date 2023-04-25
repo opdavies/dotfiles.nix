9289ace4c38b5a35bb0bda90592a08d7a039d14f
Oliver Davies <oliver@oliverdavies.uk>
2023-04-26 00:04:44 +0100

    chore(nvim): tweak indent line colours

--- config/neovim/after/plugin/indent-blankline.lua
+++ config/neovim/after/plugin/indent-blankline.lua
@@ -4,15 +4,10 @@ if not status_ok then
 end
 
 vim.opt.list = true
-vim.opt.listchars = {
-  eol = "↴",
-}
 
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#555555 gui=nocombine]]
+vim.api.nvim_set_hl(0, 'IndentBlankLineChar', { fg = "#2F313C" })
 
 indent_blankline.setup {
-  char_highlight_list = {
-    "IndentBlanklineIndent1",
-  },
   show_end_of_line = true,
 }
+
