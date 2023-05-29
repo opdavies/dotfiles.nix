e582b41bde7cfeb77ecd35a8903891bd2112cf63
Oliver Davies <oliver@oliverdavies.dev>
2023-05-30 00:57:53 +0100

    chore(sway): updates

--- config/sway/status.sh
+++ config/sway/status.sh
@@ -1,5 +1,5 @@
-date_formatted=$(date "+%d/%m/%Y - %l:%M %p - ")
+date_formatted=$(date "+%d/%m/%Y %H:%M:%S")
 
 battery_status=$(cat /sys/class/power_supply/BAT0/capacity)
 
-echo $battery_status% - $date_formatted
+echo $battery_status% $date_formatted
