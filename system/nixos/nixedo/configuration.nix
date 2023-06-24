db8bac91e7499c5069e743df99046a63fd643df7
Oliver Davies <oliver@oliverdavies.uk>
2023-06-24 10:31:51 +0100

    feat(sway): set as the default graphical session

--- system/nixos/nixedo/configuration.nix
+++ system/nixos/nixedo/configuration.nix
@@ -72,8 +72,8 @@ in
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
+  services.xserver.displayManager.defaultSession = "sway";
   services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
