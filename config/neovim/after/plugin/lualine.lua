0580fd54dcdcfe0d5a12f8c774aa8cae5db4e339
Oliver Davies <oliver@oliverdavies.uk>
2023-07-22 09:22:46 +0100

    feat(nvim): configure lualine

--- config/neovim/after/plugin/lualine.lua
+++ config/neovim/after/plugin/lualine.lua
@@ -3,4 +3,18 @@ if not status_ok then
   return
 end
 
-lualine.setup {}
+lualine.setup {
+  options = {
+    component_separators = { left = "", right = "" },
+    extensions = { "fzf", "quickfix" },
+    icons_enabled = false,
+    section_separators = { left = "", right = "" },
+    theme = "catppuccin",
+  },
+  sections = {
+    lualine_c = {
+      "filename",
+      "lsp_progress",
+    },
+  },
+}
--- home-manager/modules/common.nix
+++ home-manager/modules/common.nix
@@ -121,6 +121,8 @@ in
       vimPlugins.harpoon
       vimPlugins.impatient-nvim
       vimPlugins.indent-blankline-nvim
+      vimPlugins.lualine-lsp-progress
+      vimPlugins.lualine-nvim
       vimPlugins.nvim-autopairs
       vimPlugins.nvim-lastplace
       vimPlugins.nvim-ts-autotag
