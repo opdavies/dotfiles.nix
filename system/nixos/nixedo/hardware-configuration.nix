ddf1ddb1b2af8663046970d04c047b61f76acdfe
Oliver Davies <oliver@oliverdavies.uk>
2023-08-07 21:41:11 +0100

    refactor(nixos): define devices by label

--- system/nixos/nixedo/hardware-configuration.nix
+++ system/nixos/nixedo/hardware-configuration.nix
@@ -17,17 +17,17 @@
   hardware.enableAllFirmware = true;
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/7c6d69ec-ba06-4ddb-b9c4-62b3994fda91";
+    device = "/dev/disk/by-label/nixos";
     fsType = "ext4";
   };
 
   fileSystems."/boot/efi" = {
-    device = "/dev/disk/by-uuid/B729-9A75";
+    device = "/dev/disk/by-label/boot";
     fsType = "vfat";
   };
 
   swapDevices =
-    [{ device = "/dev/disk/by-uuid/5db0a0e6-93fb-4d0b-8fb0-fdb3cb76b89d"; }];
+    [{ device = "/dev/disk/by-label/swap"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
