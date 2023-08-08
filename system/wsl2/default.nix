482a74a5e0bcc8e7eb8fcfbde8684b7ae0a8b3ab
Oliver Davies <oliver@oliverdavies.uk>
2023-08-08 13:43:44 +0100

    fix(wsl2): inherits, import paths, unfree packages

--- system/wsl2/default.nix
+++ system/wsl2/default.nix
@@ -1,14 +1,24 @@
 { inputs, username }:
 
 inputs.home-manager.lib.homeManagerConfiguration {
+  extraSpecialArgs = { inherit inputs; };
+
   modules = [
     {
+      nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (inputs.nixpkgs.lib.getName pkg) [
+        "intelephense"
+        "postman"
+        "tabnine"
+      ];
+    }
+
+    {
       imports = [
-        ../home-manager/modules/common.nix
-        ../home-manager/modules/git.nix
-        ../home-manager/modules/home-manager.nix
-        ../home-manager/modules/tmux.nix
-        ../home-manager/modules/zsh.nix
+        ../../home-manager/modules/common.nix
+        ../../home-manager/modules/git.nix
+        ../../home-manager/modules/home-manager.nix
+        ../../home-manager/modules/tmux.nix
+        ../../home-manager/modules/zsh.nix
       ];
     }
   ];
