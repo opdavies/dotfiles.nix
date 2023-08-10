ce6193bb7b0cb97d8ed1385f14880604f05aa2ea
Oliver Davies <oliver@oliverdavies.uk>
2023-08-11 00:25:15 +0100

    feat(nvim): add fugitive keymaps
    Add keymaps to open `git status` within Fugitive as well as push and
    pull changes within a fugitive buffer.

---
+++ config/neovim/after/plugin/fugitive.lua
@@ -0,0 +1,22 @@
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  pattern = "*",
+
+  callback = function()
+    if vim.bo.ft ~= "fugitive" then
+      return
+    end
+
+    local bufnr = vim.api.nvim_get_current_buf()
+    local opts = { buffer = bufnr, remap = false }
+
+    vim.keymap.set("n", "<leader>p", function()
+      vim.cmd.Git('push')
+    end, opts)
+
+    vim.keymap.set("n", "<leader>P", function()
+      vim.cmd.Git({'pull',  '--rebase'})
+    end, opts)
+  end,
+})
