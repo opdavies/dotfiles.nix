9bf205edd8996479ed1489f84c707c01206d57ce
Oliver Davies <oliver@oliverdavies.uk>
2023-08-14 00:50:47 +0100

    feat(nvim): add more Harpoon keymaps

--- config/neovim/after/plugin/harpoon.lua
+++ config/neovim/after/plugin/harpoon.lua
@@ -10,6 +10,9 @@ harpoon.setup {}
 nmap { "<M-h><M-l>", require("harpoon.ui").toggle_quick_menu }
 nmap { "<M-h><M-m>", require("harpoon.mark").add_file }
 
+nmap { "<leader>hl", require("harpoon.ui").toggle_quick_menu }
+nmap { "<leader>hm", require("harpoon.mark").add_file }
+
 for i = 1, 5 do
   nmap {
     string.format("<space>%s", i),
