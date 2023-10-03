80d9347a735cf8079db8353908afec36d6b9409b
Oliver Davies <oliver@oliverdavies.uk>
2023-10-03 18:19:01 +0100

    chore(nvim): remove `drupalclass`
    This has been replaced by generation tools within Phpactor.

--- config/neovim/lua/opdavies/snippets/ft/php.lua
+++ config/neovim/lua/opdavies/snippets/ft/php.lua
@@ -8,59 +8,6 @@ local t = ls.text_node
 
 local M = {
 
-  drupalclass = fmta(
-    [[
-    <<?php
-
-    declare(strict_types=1);
-
-    namespace <>;
-
-    final class <> {
-
-      <>
-
-    }]],
-    {
-      f(function()
-        local filepath = vim.fn.expand "%:h"
-        local filepath_parts = vim.fn.split(filepath, "/")
-
-        if not vim.tbl_contains(filepath_parts, "src") then
-          return ""
-        end
-
-        local namespace_parts = { "Drupal" }
-
-        local is_test_file = vim.tbl_contains(filepath_parts, "tests")
-        if is_test_file then
-          table.insert(namespace_parts, "Tests")
-        end
-
-        -- Find and add the module name.
-        for k, v in ipairs(filepath_parts) do
-          if v == "src" then
-            if is_test_file then
-              table.insert(namespace_parts, filepath_parts[k - 2])
-            else
-              table.insert(namespace_parts, filepath_parts[k - 1])
-            end
-          end
-        end
-
-        -- Add the rest of the namespace.
-        local namespace = vim.split(filepath, "src/")
-        local final_part = (namespace[2] or ""):gsub("/", "\\")
-        table.insert(namespace_parts, final_part)
-
-        return table.concat(namespace_parts, "\\")
-      end),
-      f(function()
-        return vim.fn.expand "%:t:r"
-      end),
-      i(0),
-    }
-  ),
 
   func = fmta("function <>(<>)<> {\n  <>\n}<>", { i(1), i(2), i(3), i(4), i(0) }),
 
