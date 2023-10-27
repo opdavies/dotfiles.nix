941d9951bd0f0f170dd1b660fd3a3e8f7b57635d
Oliver Davies <oliver@oliverdavies.uk>
2023-10-27 21:12:23 +0100

    feat(nvim): configure cmdline completion

--- config/neovim/after/plugin/completion.lua
+++ config/neovim/after/plugin/completion.lua
@@ -67,6 +67,22 @@ cmp.setup {
   },
 }
 
+cmp.setup.cmdline({ "/", "?" }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = "buffer" },
+  },
+})
+
+cmp.setup.cmdline(":", {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = "path" },
+  }, {
+    { name = "cmdline" },
+  }),
+})
+
 vim.cmd [[
   augroup DadbodSql
     au!
