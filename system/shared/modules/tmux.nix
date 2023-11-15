f44231d595ba709a0493a4d11f7f20ff7daebac4
Oliver Davies <oliver@oliverdavies.uk>
2023-11-15 12:51:18 +0000

    chore(tmux): update status line

--- system/shared/modules/tmux.nix
+++ system/shared/modules/tmux.nix
@@ -33,15 +33,14 @@
       bind -n C-Up resize-pane -U 5
 
       # Status line customisation
-      set-option -g status-left-length 100
+      set-option -g status-left "  "
+      set-option -g status-right " #{session_name} "
       set-option -g status-right-length 100
-      set-option -g status-left " #{session_name}  "
-      set-option -g status-right "#{pane_title} "
       set-option -g status-style "fg=#7C7D83 bg=#242631"
-      set-option -g window-status-format "#{window_index}:#{pane_current_command}#{window_flags} "
+      set-option -g window-status-activity-style none
       set-option -g window-status-current-format "#{window_index}:#{pane_current_command}#{window_flags} "
       set-option -g window-status-current-style "fg=#E9E9EA"
-      set-option -g window-status-activity-style none
+      set-option -g window-status-format "#{window_index}:#{pane_current_command}#{window_flags} "
 
       bind c new-window -c "#{pane_current_path}"
 
