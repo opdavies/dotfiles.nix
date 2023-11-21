024384303f34c9bf7cdf63e3ab5215ae1f5e2246
Oliver Davies <oliver@oliverdavies.uk>
2023-11-21 23:16:17 +0000

    feat(i3): more key mappings

--- system/nixos/home-manager/modules/i3.nix
+++ system/nixos/home-manager/modules/i3.nix
@@ -45,6 +45,25 @@ in
       keybindings = inputs.nixpkgs.lib.mkOptionDefault {
         "${modifier}+Shift+b" = "exec firefox";
         "${modifier}+Tab" = "workspace back_and_forth";
+
+        # Change focus.
+        "${modifier}+h" = "focus left";
+        "${modifier}+j" = "focus down";
+        "${modifier}+k" = "focus up";
+        "${modifier}+l" = "focus right";
+
+        # Move focused window.
+        "${modifier}+Shift+h" = "move left";
+        "${modifier}+Shift+j" = "move down";
+        "${modifier}+Shift+k" = "move up";
+        "${modifier}+Shift+l" = "move right";
+
+        "XF86AudioRaiseVolume" = "exec pamixer -ui 2 && pamixer --get-volume";
+        "XF86AudioLowerVolume" = "exec pamixer -ud 2 && pamixer --get-volume";
+        "XF86AudioMute" = "exec pamixer --toggle-mute && ( [ \"$(pamixer --get-mute)\" = \"true\" ] && echo 0";
+
+        "XF86MonBrightnessDown" = "exec brightnessctl set 5%- | sed -En 's/.*\(([0-9]+)%\).*/\1/p'";
+        "XF86MonBrightnessUp" = "exec brightnessctl set +5% | sed -En 's/.*\(([0-9]+)%\).*/\1/p'";
       };
 
       terminal = "alacritty";
