bad8b22a3bb935d46edd1b633bf8b4207eb0bdec
Oliver Davies <oliver@oliverdavies.uk>
2023-12-11 17:45:36 +0000

    Upgrade to Harpoon 2

--- config/neovim/after/plugin/harpoon.lua
+++ config/neovim/after/plugin/harpoon.lua
@@ -1,23 +1,26 @@
-local status_ok, harpoon = pcall(require, "harpoon")
-if not status_ok then
-  return
-end
-
+local harpoon = require "harpoon"
 local nmap = require("opdavies.keymap").nmap
 
-harpoon.setup {}
+harpoon:setup()
 
-nmap { "<M-h><M-l>", require("harpoon.ui").toggle_quick_menu }
-nmap { "<M-h><M-m>", require("harpoon.mark").add_file }
-
-nmap { "<leader>hl", require("harpoon.ui").toggle_quick_menu }
-nmap { "<leader>hm", require("harpoon.mark").add_file }
+nmap {
+  "<leader>hl",
+  function()
+    harpoon.ui:toggle_quick_menu(harpoon:list())
+  end,
+}
+nmap {
+  "<leader>hm",
+  function()
+    harpoon:list():append()
+  end,
+}
 
 for i = 1, 5 do
   nmap {
     string.format("<space>%s", i),
     function()
-      require("harpoon.ui").nav_file(i)
+      harpoon:list():select(i)
     end,
   }
 end
