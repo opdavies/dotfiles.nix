d5b2d76dfbbd0c16d008e211ad8f16acc25fb199
Oliver Davies <oliver@oliverdavies.uk>
2024-01-07 21:56:59 +0000

    Revert "Automatically close brackets, parentheses and"
    This reverts commit 799ca92f401b6b11bebbb7bd514214b9156e2504.

--- config/neovim/lua/opdavies/init.lua
+++ config/neovim/lua/opdavies/init.lua
@@ -8,3 +8,7 @@ require "opdavies.lsp"
 
 require "opdavies.telescope.setup"
 require "opdavies.telescope.mappings"
+
+vim.api.nvim_create_user_command("GoToFile", function()
+  require("opdavies.telescope").git_files()
+end, {})
