34eddc3e3ea475b034a7c98abdf196e22f1b1d53
Oliver Davies <oliver@oliverdavies.dev>
2024-02-11 10:40:48 +0000

    Remove nixpkgs 23.11
    As it's no longer used in my dotfiles.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   inputs = {
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
-    nixpkgs-2311.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     opdavies-nvim.inputs.nixpkgs.follows = "nixpkgs";
     opdavies-nvim.url = "github:opdavies/opdavies.nvim";
