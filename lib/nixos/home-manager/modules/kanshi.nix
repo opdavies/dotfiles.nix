859d46781ce009f2fd90eaaef033189ab89e2a97
Oliver Davies <oliver@oliverdavies.dev>
2024-05-31 16:09:49 +0100

    Update `kanshi.settings`

--- lib/nixos/home-manager/modules/kanshi.nix
+++ lib/nixos/home-manager/modules/kanshi.nix
@@ -10,18 +10,21 @@
     enable = true;
 
     # swaymsg -t get_outputs
-    settings = {
-      undocked = {
-        outputs = [{
-          criteria = "eDP-1";
-          status = "enable";
-          mode = "1920x1080";
-          position = "0,0";
-        }];
-      };
-
-      docked = {
-        outputs = [
+    settings = [
+      {
+        profile.name = "undocked";
+        profile.outputs = [
+          {
+            criteria = "eDP-1";
+            status = "enable";
+            mode = "1920x1080";
+            position = "0,0";
+          }
+        ];
+      }
+      {
+        profile.name = "docked";
+        profile.outputs = [
           {
             criteria = "eDP-1";
             status = "disable";
@@ -31,9 +34,10 @@
             criteria = "Samsung Electric Company LU28R55 H4ZRA00128";
             mode = "3840x2160";
             position = "0,0";
+            scale = 1.5;
           }
         ];
-      };
-    };
+      }
+    ];
   };
 }
