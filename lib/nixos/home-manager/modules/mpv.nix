a3bcd63b3db64250e34d9a1d8a4c78b34e17212f
Oliver Davies <oliver@oliverdavies.dev>
2024-05-31 19:46:47 +0100

    Add keybindings for controlling volume
    Add keybindings for increasing and decreasing volume in mpv.

--- lib/nixos/home-manager/modules/mpv.nix
+++ lib/nixos/home-manager/modules/mpv.nix
@@ -1,5 +1,11 @@
 {
   programs.mpv = {
     enable = true;
+
+    # https://github.com/mpv-player/mpv/blob/master/etc/input.conf
+    bindings = {
+      "DOWN" = "add volume -5";
+      "UP" = "add volume 5";
+    };
   };
 }
