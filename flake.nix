8ad1637c57754381303a8c95659b09b5b5b0288e
Oliver Davies <oliver@oliverdavies.dev>
2024-06-11 07:37:49 +0100

    Switch from NixOS unstable to 24.05 by default

--- flake.nix
+++ flake.nix
@@ -1,9 +1,11 @@
 {
   inputs = {
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     opdavies-nvim.inputs.nixpkgs.follows = "nixpkgs";
     opdavies-nvim.url = "github:opdavies/opdavies.nvim";
