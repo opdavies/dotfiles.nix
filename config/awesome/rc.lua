3137c0e096fc72db16623c3731834abd8fa70e14
Oliver Davies <oliver@oliverdavies.dev>
2024-07-03 08:00:00 +0100

    awesome: make `max` the default layout

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -73,8 +73,8 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-  awful.layout.suit.tile,
   awful.layout.suit.max,
+  awful.layout.suit.tile,
   awful.layout.suit.floating,
   -- awful.layout.suit.tile.left,
   -- awful.layout.suit.tile.bottom,
