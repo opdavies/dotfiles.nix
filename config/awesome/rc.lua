8f98a24ea1cd1cc8739934819b89d4f9fab22c28
Oliver Davies <oliver@oliverdavies.dev>
2024-07-03 08:00:00 +0100

    awesome: add spacing between keybinds

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -268,9 +268,13 @@ root.buttons(gears.table.join(
 -- {{{ Key bindings
 globalkeys = gears.table.join(
   awful.key({ modkey }, "s", hotkeys_popup.show_help, { description = "show help", group = "awesome" }),
+
   awful.key({ modkey }, "Left", awful.tag.viewprev, { description = "view previous", group = "tag" }),
+
   awful.key({ modkey }, "Right", awful.tag.viewnext, { description = "view next", group = "tag" }),
+
   awful.key({ modkey }, "Escape", awful.tag.history.restore, { description = "go back", group = "tag" }),
+
   awful.key({ modkey }, "j", function()
     awful.client.focus.byidx(-1)
   end, { description = "focus previous by index", group = "client" }),
@@ -301,6 +305,7 @@ globalkeys = gears.table.join(
   end, { description = "focus the next screen", group = "screen" }),
 
   awful.key({ modkey }, "u", awful.client.urgent.jumpto, { description = "jump to urgent client", group = "client" }),
+
   awful.key({ modkey }, "Tab", function()
     awful.client.focus.history.previous()
     if client.focus then
@@ -312,29 +317,39 @@ globalkeys = gears.table.join(
   awful.key({ modkey }, "Return", function()
     awful.spawn(terminal)
   end, { description = "open a terminal", group = "launcher" }),
+
   awful.key({ modkey, "Control" }, "r", awesome.restart, { description = "reload awesome", group = "awesome" }),
+
   awful.key({ modkey, "Shift" }, "q", awesome.quit, { description = "quit awesome", group = "awesome" }),
+
   awful.key({ modkey }, "l", function()
     awful.tag.incmwfact(0.05)
   end, { description = "increase master width factor", group = "layout" }),
+
   awful.key({ modkey }, "h", function()
     awful.tag.incmwfact(-0.05)
   end, { description = "decrease master width factor", group = "layout" }),
+
   awful.key({ modkey, "Shift" }, "h", function()
     awful.tag.incnmaster(1, nil, true)
   end, { description = "increase the number of master clients", group = "layout" }),
+
   awful.key({ modkey, "Shift" }, "l", function()
     awful.tag.incnmaster(-1, nil, true)
   end, { description = "decrease the number of master clients", group = "layout" }),
+
   awful.key({ modkey, "Control" }, "h", function()
     awful.tag.incncol(1, nil, true)
   end, { description = "increase the number of columns", group = "layout" }),
+
   awful.key({ modkey, "Control" }, "l", function()
     awful.tag.incncol(-1, nil, true)
   end, { description = "decrease the number of columns", group = "layout" }),
+
   awful.key({ modkey }, "space", function()
     awful.layout.inc(1)
   end, { description = "select next", group = "layout" }),
+
   awful.key({ modkey, "Shift" }, "space", function()
     awful.layout.inc(-1)
   end, { description = "select previous", group = "layout" }),
@@ -538,6 +553,7 @@ awful.rules.rules = {
         "Wpa_gui",
         "veromix",
         "xtightvncviewer",
+        "zoom",
       },
 
       -- Note that the name property shown in xprop might be set slightly after creation of the client
