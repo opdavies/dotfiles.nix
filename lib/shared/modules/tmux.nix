0fc3c765e57a3bcc70c6f7ee478481a21515a9bd
Oliver Davies <oliver@oliverdavies.dev>
2024-07-05 23:10:56 +0100

    tmux: remove padding around words in the status
    ...bar

--- lib/shared/modules/tmux.nix
+++ lib/shared/modules/tmux.nix
@@ -36,8 +36,8 @@ in
       bind -n C-Up resize-pane -U 5
 
       # Status line customisation
-      set-option -g status-left " "
-      set-option -g status-right " #{session_name} "
+      set-option -g status-left ""
+      set-option -g status-right " #{session_name}"
       set-option -g status-right-length 100
       set-option -g status-style "fg=#7C7D83 bg=default"
       set-option -g window-status-activity-style none
