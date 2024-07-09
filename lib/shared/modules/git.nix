5bf2253f98770c3c8a2f75040ef91b8c11d5deac
Oliver Davies <oliver@oliverdavies.dev>
2024-07-09 14:25:00 +0100

    git: organise extraConfig values

--- lib/shared/modules/git.nix
+++ lib/shared/modules/git.nix
@@ -105,40 +105,47 @@
           autosetuprebase = "always";
           sort = "-committerdate";
         };
+
         checkout.defaultRemote = "origin";
         color.ui = true;
         column.ui = "auto";
-        commit = {
-          template = "~/.gitmessage";
-        };
+        commit.template = "~/.gitmessage";
+
         core = {
           editor = "nvim";
           excludesFile = "~/.config/git/ignore";
           pager = "delta";
         };
+
         delta.line-numbers = true;
         fetch.prune = true;
         gpg.format = "ssh";
         grep.lineNumber = true;
         help.autocorrect = "1";
         init.defaultBranch = "main";
+
         maintenance = {
           auto = false;
           strategy = "incremental";
         };
+
         merge.ff = "only";
+
         push = {
           autoSetupRemote = true;
           default = "upstream";
         };
+
         pull = {
           ff = "only";
           rebase = true;
         };
+
         rebase = {
           autosquash = true;
           autostash = true;
         };
+
         user.signingkey = "~/.ssh/id_rsa.pub";
       };
     };
