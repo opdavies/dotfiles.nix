8c095288dc0c7a8ecc6c0442259110275f8574a1
Oliver Davies <oliver@oliverdavies.dev>
2024-07-21 00:28:35 +0100

    espanso: move matches into a separate file

---
+++ lib/nixos/home-manager/modules/espanso/matches.nix
@@ -0,0 +1,66 @@
+{ baseUrl }:
+
+{
+  matches = [
+    {
+      trigger = ":archive";
+      replace = "${baseUrl}/archive";
+    }
+    {
+      trigger = ":atdc";
+      replace = "${baseUrl}/atdc";
+    }
+    {
+      trigger = ":call";
+      replace = "${baseUrl}/call";
+    }
+    {
+      trigger = ":coaching";
+      replace = "${baseUrl}/team-coaching";
+    }
+    {
+      trigger = ":daily";
+      replace = "${baseUrl}/daily";
+    }
+    {
+      trigger = ":dotfiles";
+      replace = "https://github.com/opdavies/dotfiles.nix";
+    }
+    {
+      trigger = ":dc";
+      replace = "Drupal Commerce";
+    }
+    {
+      trigger = ":dr";
+      replace = "Drupal";
+    }
+    {
+      trigger = ":gt";
+      replace = "Great, thanks!";
+    }
+    {
+      trigger = ":lh";
+      replace = "http://localhost";
+    }
+    {
+      trigger = ":podcast";
+      replace = "${baseUrl}/podcast";
+    }
+    {
+      trigger = ":pricing";
+      replace = "${baseUrl}/pricing";
+    }
+    {
+      trigger = ":talks";
+      replace = "${baseUrl}/talks";
+    }
+    {
+      trigger = ":website";
+      replace = "${baseUrl}";
+    }
+    {
+      trigger = ":zoom";
+      replace = "https://savvycal.com/opdavies/zoom";
+    }
+  ];
+}
