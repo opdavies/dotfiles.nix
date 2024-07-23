b661f78a28098e77068d178144fde50eec068e70
Oliver Davies <oliver@oliverdavies.dev>
2024-07-23 21:27:07 +0100

    awesome: remove `s.mylayoutbox`
    It's not needed if I only have one layout option enabled.

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -249,7 +249,7 @@ awful.screen.connect_for_each_screen(function(s)
       wibox.widget.systray(),
       require "widgets.battery",
       mytextclock,
-      s.mylayoutbox,
+      -- s.mylayoutbox,
     },
   }
 end)
