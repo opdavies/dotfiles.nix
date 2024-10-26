cffba6b6b6ab0a9d82b6b8533936e1d2f4eb134d
Oliver Davies <oliver@oliverdavies.dev>
2024-10-26 15:45:29 +0100

    Re-add en.utf-8.add spellfile

--- lib/shared/modules/zsh/aliases.nix
+++ lib/shared/modules/zsh/aliases.nix
@@ -9,6 +9,6 @@
   s = "secrets";
   secrets = ''doppler --project "$(whoami)" run'';
   tag = "tag-release";
-  vss = "LC_ALL=C sort --unique $REPOS/opdavies.nvim/spell/en.utf-8.add --output $REPOS/opdavies.nvim/spell/en.utf-8.add";
+  vss = "LC_ALL=C sort --unique $REPOS/dotfiles.nix/config/neovim/spell/en.utf-8.add --output $REPOS/dotfiles.nix/config/neovim/spell/en.utf-8.add";
   wt = "git worktree";
 }
