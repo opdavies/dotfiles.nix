38fb5ddb25c45057d08978d6257abd051cc97f76
Oliver Davies <oliver@oliverdavies.dev>
2024-10-29 12:00:00 +0000

    Use `gofmt` as a formatter for go files

--- config/neovim/plugin/conform.lua
+++ config/neovim/plugin/conform.lua
@@ -3,6 +3,7 @@ local conform = require "conform"
 conform.setup {
   formatters_by_ft = {
     bash = { "shellcheck" },
+    go = { "gofmt" },
     javascript = { { "prettierd", "prettier" } },
     just = { "just" },
     lua = { "stylua" },
