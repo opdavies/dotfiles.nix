23bad15ab1157629a187125ead08dcdb7277e2aa
Oliver Davies <oliver@oliverdavies.dev>
2024-11-16 20:37:27 +0000

    Add terminal settings

--- config/neovim/lua/opdavies/keymaps.lua
+++ config/neovim/lua/opdavies/keymaps.lua
@@ -107,3 +107,9 @@ set("n", "<leader>et", function()
 end)
 
 set("n", "<leader>ec", ":edit composer.json")
+
+-- These mappings control the size of splits (height/width).
+set("n", "<M-,>", "<c-w>5<")
+set("n", "<M-.>", "<c-w>5>")
+set("n", "<M-t>", "<C-W>+")
+set("n", "<M-s>", "<C-W>-")
