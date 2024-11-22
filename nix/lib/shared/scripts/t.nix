e5b60ccc8b56ae0585afa8bea0ad8c75964b0e47
Oliver Davies <oliver@oliverdavies.dev>
2024-11-22 01:19:15 +0000

    Add ~/Code/personal directory

--- nix/lib/shared/scripts/t.nix
+++ nix/lib/shared/scripts/t.nix
@@ -16,7 +16,7 @@
     else
       # Get the session name from fuzzy-finding list of directories and generating a
       # tmux-safe version.
-      items=$(find "$REPOS" ~/Documents \
+      items=$(find "$REPOS" "$REPOS/personal" ~/Documents \
         -maxdepth 1 -mindepth 1 -type d \
         ! -name "_archive" \
         ! -name "*-old" \
