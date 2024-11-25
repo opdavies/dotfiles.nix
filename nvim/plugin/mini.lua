4c5fd19f5ae2f485e6e379ff23d29f69deed389b
Oliver Davies <oliver@oliverdavies.dev>
2024-11-25 08:03:59 +0000

    Remove mini.statusline

--- nvim/plugin/mini.lua
+++ nvim/plugin/mini.lua
@@ -15,9 +15,4 @@ require("mini.move").setup {}
 
 require("mini.operators").setup {}
 
-require("mini.statusline").setup {
-  set_vim_settings = false,
-  use_icons = false,
-}
-
 require("mini.surround").setup {}
