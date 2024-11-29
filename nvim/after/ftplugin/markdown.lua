d79350f36a66fb72a19c6ae6cef5e9486316f669
Oliver Davies <oliver@oliverdavies.dev>
2024-11-29 19:24:55 +0000

    Remove reference to treesitter-context

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,5 +1,3 @@
 -- TODO: Interim fix for https://github.com/nvim-treesitter/nvim-treesitter-context/issues/431.
-require("treesitter-context").disable()
-
 vim.opt_local.spell = true
 vim.opt_local.wrap = true
