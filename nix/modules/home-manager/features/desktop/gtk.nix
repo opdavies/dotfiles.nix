aa75621f4c263d3a658f4c5cd4431f707eb151f0
Oliver Davies <oliver@oliverdavies.dev>
2024-12-08 22:41:55 +0000

    Disable animations

--- nix/modules/home-manager/features/desktop/gtk.nix
+++ nix/modules/home-manager/features/desktop/gtk.nix
@@ -21,6 +21,9 @@
         name = "Arc";
         package = pkgs.arc-icon-theme;
       };
+
+      gtk3.extraConfig.gtk-enable-animations = false;
+      gtk4.extraConfig.gtk-enable-animations = false;
     };
   };
 }
