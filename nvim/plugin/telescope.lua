58f645ee507f403dd5ae1f6db4712881cb6a002d
Oliver Davies <oliver@oliverdavies.dev>
2024-12-12 22:04:23 +0000

    Add multigrep from TJ's video

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -50,7 +50,6 @@ end
 vim.keymap.set("n", "<space>/", builtin.current_buffer_fuzzy_find)
 vim.keymap.set("n", "<space>fb", builtin.buffers)
 vim.keymap.set("n", "<space>fd", builtin.find_files)
-vim.keymap.set("n", "<space>fg", builtin.live_grep)
 vim.keymap.set("n", "<space>fh", builtin.help_tags)
 vim.keymap.set("n", "<space>fi", M.search_all_files)
 vim.keymap.set("n", "<space>fk", builtin.keymaps)
@@ -65,3 +64,5 @@ vim.keymap.set("n", "<space>dgb", M.grep_bluecheese)
 vim.keymap.set("n", "<space>dgd", M.grep_drupalorg_theme)
 
 vim.keymap.set({ "n", "v" }, "<space>gw", builtin.grep_string)
+
+require("opdavies.telescope.multigrep").setup {}
